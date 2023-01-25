12b298d9d266eb68c1edbc2a89006612a9f7b24c
Pavitar Sharma <76960865+PavitarSharma@users.noreply.github.com>
2023-01-25 21:08:03 +0530

    Add files via upload

--- longest -common-substring.js
+++ longest -common-substring.js
@@ -1,21 +1,17 @@
-function longestCommonSubstring(string1, string2, m, n) {
-    let lcStuff = Array(m+1).fill().map(() => Array(n+1).fill(0))
+function longestCommonSubstring(text1, text2) {
+    let lcSub = new Array(text1.length+1).fill(null).map(() => new Array(text2.length+1).fill(0));
 
-    let res = 0
-    for(let i = 0; i<= m; i++){
-        for(let j = 0; j <=n; j++) {
-            if(i == 0 || j == 0) {
-                lcStuff[i][j] = 0
-            }else if(string1[i-1] == string2[j - 1]) {
-                lcStuff[i][j] = lcStuff[i-1][j-1] + 1
-                res = Math.max(res, lcStuff[i][j])
-            }else {
-                lcStuff[i][j] = 0
+    for (let i = 1; i <= text1.length; i++) {
+        for (let j = 1; j <= text2.length; j++) {
+            if (text1[i-1] === text2[j-1]) {
+                lcSub[i][j] = lcSub[i-1][j-1] + 1;
+            } else {
+                lcSub[i][j] = Math.max(lcSub[i-1][j], lcSub[i][j-1]);
             }
         }
     }
-
-    return res
+    
+    return lcSub[text1.length][text2.length];
 }
 
 const string1 = "Pavitar"
